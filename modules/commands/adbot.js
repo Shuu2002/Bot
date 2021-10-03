199e44c34e5c7ecc7a542c1cd6f050a4a45587c0
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 01:34:21 +0700

    Update adbot.js

--- modules/commands/adbot.js
+++ modules/commands/adbot.js
@@ -16,10 +16,10 @@ module.exports.config = {
 
 module.exports.run = async({api,event,args,Users,global,Currencies}) => {
 var callback = () => api.sendMessage(
-  {body:`Nguyễn Quốc Vinh`,
+  {body:`Tử Thần Đau Khổ`,
     attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(
-        encodeURI(`https://graph.facebook.com/${100005303558824}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`)).pipe(
+        encodeURI(`https://graph.facebook.com/${100069432743803}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
        }
