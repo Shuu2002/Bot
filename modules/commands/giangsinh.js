018ef7dbffd15eaf3467ac9f40c971f820b0fff2
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 16:00:49 +0700

    Update giangsinh.js

--- modules/commands/giangsinh.js
+++ modules/commands/giangsinh.js
@@ -4,12 +4,12 @@
 	hasPermssion: 0,
 	credits: "CatalizCS",
 	description: "Đếm ngược ngày Noel",
-        commandCategory: "game",
+        commandCategory: "đếm ngược",
 	cooldowns: 5
 }
 
 module.exports.run = function ({ event, api }) {
-    const t = Date.parse("December 25, 2021 00:00:00") - Date.parse(new Date());
+    const t = Date.parse("December 25, 2022 00:00:00") - Date.parse(new Date());
     const seconds = Math.floor( (t/1000) % 60 );
     const minutes = Math.floor( (t/1000/60) % 60 );
     const hours = Math.floor( (t/(1000*60*60)) % 24 );
