3d179cc9f9e66f63edca7c7c657acd2d8fbd3cda
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 13:42:58 +0700

    Update đấm.js

--- modules/commands/đấm.js
+++ modules/commands/đấm.js
@@ -6,7 +6,7 @@ module.exports.config = {
   hasPermssion: 0,
   credits: "Vinhbeat",
   description: "đấm",
-  commandCategory: "Vinhbeat",
+  commandCategory: "hành động",
   usages: "[tag người]",
   cooldowns: 5,
 };
