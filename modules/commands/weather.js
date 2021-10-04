b64e2377c20d9d5bf05f6f78284eb7ad2c97cded
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 18:36:52 +0700

    Update weather.js

--- modules/commands/weather.js
+++ modules/commands/weather.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "Mirai Team",
 	description: "Xem thông tin thời tiết tại khu vực",
-	commandCategory: "other",
+	commandCategory: "group",
 	usages: "[Location]",
 	cooldowns: 5,
 	dependencies: {
