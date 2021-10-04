23d1223a14133325e990cacd5181f584d7df0a9b
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 16:01:16 +0700

    Update hanna.js

--- modules/commands/hanna.js
+++ modules/commands/hanna.js
@@ -2,7 +2,7 @@ const request = require("request");
 const fs = require("fs")
 const axios = require("axios")
 module.exports.config = {
-  name: "hannaUwU",
+  name: "hanna",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "HungCho",
