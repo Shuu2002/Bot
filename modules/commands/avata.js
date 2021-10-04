e67fbae8ca6595b8d3ad1aee72e99101dde33816
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 13:46:28 +0700

    Update avata.js

--- modules/commands/avata.js
+++ modules/commands/avata.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "Jukie~",
 	description: "Ảnh", //nhập thứ bạn muốn
-	commandCategory: "Phương tiện", //Phần hiển thị trên help
+	commandCategory: "hình ảnh", //Phần hiển thị trên help
 	usages: "", //cách sử dụng
 	cooldowns: 1, //thời gian chờ cách nhau
 	
