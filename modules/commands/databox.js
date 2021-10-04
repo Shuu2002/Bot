badbe6b1316a21c0aa20647aa3af21c91ed02041
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 21:18:26 +0700

    Update databox.js

--- modules/commands/databox.js
+++ modules/commands/databox.js
@@ -1,7 +1,7 @@
 ﻿module.exports.config = {
     name: "databox",
     version: "0.0.1-beta",
-    hasPermssion: 2,
+    hasPermssion: 0,
     credits: "Lợi",
     description: "tìm kiếm thông tin qua id của box hoặc người dùng",
     commandCategory: "dành cho Admin",
