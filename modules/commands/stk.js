23b22703c0c220bef2af9af7a16c929811925612
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 20:05:55 +0700

    Update stk.js

--- modules/commands/stk.js
+++ modules/commands/stk.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("stk")==0 || (event.body.indexOf("STK")==0)) {
 		var msg = {
-				body: "Hiện chưa có stk",
+				body: "Mình chạy bot hoàn toàn free nhưng nếu bạn nào có lòng tốt muốn ủng hộ mình bằnh bank hoặc card theo:\n\n\n🐳Chuyển khoản:19037171494011-TCB\n🐳Zalo:0869189602\n🐳facebook:https://www.facebook.com/profile.php?id=100056649937015\nCảm ơn các bạn đã ủng hộ:33.\nChúc các bạn trải nghiệm bot vui vẻ!!",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
