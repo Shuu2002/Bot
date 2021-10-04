86ab769d4b0ecb3d99dc42b3dd4ad796c1ab54d5
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 20:29:11 +0700

    Update 8ball.js

--- modules/commands/8ball.js
+++ modules/commands/8ball.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "VanHung",
 	description: "Lệnh vô dụng",
-	commandCategory: "hình ảnh",
+	commandCategory: "lệnh xàm",
 	usages: "boy",
 	cooldowns: 5
 };
