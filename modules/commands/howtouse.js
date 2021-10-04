952507985108968644f84f00aa464f56896c065a
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 16:27:39 +0700

    Update howtouse.js

--- modules/commands/howtouse.js
+++ modules/commands/howtouse.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "Kanzu",
 	description: "*",
-	commandCategory: "Box - Chat",
+	commandCategory: "cách dùng",
 	usages: "howtouse option input",
 	cooldowns: 5,
 	info: [
