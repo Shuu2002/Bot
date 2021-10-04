9c9c3137559b30ad258ca90189e0d8dd3b24d202
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 13:33:00 +0700

    Update coco.js

--- modules/commands/coco.js
+++ modules/commands/coco.js
@@ -4,7 +4,7 @@ module.exports.config = {
  hasPermssion: 0,
  credits: "JRT",
  description: "Random ảnh pekora",
- commandCategory: "random-img",
+ commandCategory: "hololive",
  usages: "coco",
  cooldowns: 3
 };
