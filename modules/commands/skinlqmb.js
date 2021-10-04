66908e056344ce1e2c1d98a4722a2c0d0b337f6f
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-04 22:41:08 +0700

    Update skinlqmb.js

--- modules/commands/skinlqmb.js
+++ modules/commands/skinlqmb.js
@@ -4,7 +4,7 @@ module.exports.config = {
   hasPermssion: 0,
   credits: "HĐGN",
   description: "Random Ảnh Skin Liên Quân",
-  commandCategory: "LIÊN QUÂN MOBILE",
+  commandCategory: "hình ảnh",
   usages: "",
   cooldowns: 0,
   dependencies: {
