e8067ec117b99b39ac404d86e0d1530750f83654
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-05 08:12:42 +0700

    Update thính.js

--- modules/commands/thính.js
+++ modules/commands/thính.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "VĐT&NTH",
 	description: "Ca Dao Việt Nam",
-	commandCategory: "Command",
+	commandCategory: "thơ",
 	usages: "gai",
 	cooldowns: 5
 };
