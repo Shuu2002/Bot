b343e5994f8fe7e10f89d786422e0967b8d37323
¯\_(ツ)_/¯ <88658626+Textj@users.noreply.github.com>
2021-10-10 17:37:11 +0700

    Update rank.js

--- modules/commands/rank.js
+++ modules/commands/rank.js
@@ -1,22 +1,18 @@
 module.exports.config = {
-	name: "rank",
-	version: "1.0.3",
-	hasPermssion: 0,
-	credits: "CataliCS",
-	description: "Lấy rank hiện tại của bạn trên hệ thống bot, remake rank_card from canvacord",
-	commandCategory: "system",
-	cooldowns: 20,
-	dependencies: {
-		"fs-extra": "",
-		"path": "",
-		"jimp": "",
-		"node-superfetch": "",
-		"canvas": "",
-		"@miraipr0ject/assets": ""
-	},
-	envConfig: {
-		APIKEY: "571752207151901|AC-zG86sv6U6kpnT0_snIHBOHJc"
-	}
+ name: "rank",
+ version: "1.0.0",
+ hasPermssion: 0,
+ credits: "CataliCS",
+ description: "Lấy rank hiện tại của bạn trên hệ thống bot kèm khung theo level của bạn, remake rank_card from canvacord",
+ commandCategory: "Thông tin",
+ cooldowns: 0,
+ dependencies: {
+  "fs-extra": "",
+  "path": "",
+  "jimp": "",
+  "node-superfetch": "",
+  "canvas": ""
+ }
 };
 
 module.exports.makeRankCard = async (data) => {    
@@ -28,130 +24,170 @@ module.exports.makeRankCard = async (data) => {
 
     const fs = global.nodemodule["fs-extra"];
     const path = global.nodemodule["path"];
-	const Canvas = global.nodemodule["canvas"];
-	const request = global.nodemodule["node-superfetch"];
-	const __root = path.resolve(__dirname, "cache");
-	const PI = Math.PI;
+ const Canvas = global.nodemodule["canvas"];
+ const request = global.nodemodule["node-superfetch"];
+ const __root = path.resolve(__dirname, "cache");
+ const PI = Math.PI;
 
     const { id, name, rank, level, expCurrent, expNextLevel } = data;
+
+ Canvas.registerFont(__root + "/regular-font.ttf", {
+  family: "Manrope",
+  weight: "regular",
+  style: "normal"
+ });
+ Canvas.registerFont(__root + "/bold-font.ttf", {
+  family: "Manrope",
+  weight: "bold",
+  style: "normal"
+ });
+
+ const pathCustom = path.resolve(__dirname, "cache", "customrank");
+var customDir = fs.readdirSync(pathCustom);
+let random = Math.floor(Math.random() * 18) + 1;
+   var dirImage = __root + "/rankcard" + random + ".png";
+
+
+customDir = customDir.map(item => item.replace(/\.png/g, ""));
+
+for (singleLimit of customDir) {
+var limitRate = false;
+const split = singleLimit.split(/-/g);
+var min = parseInt(split[0]), max = parseInt((split[1]) ? split[1] : min);
+
+for (; min <= max; min++) {
+if (level == min) {
+limitRate = true;
+break;
+}
+}
+
+if (limitRate == true) {
+dirImage = pathCustom + `/${singleLimit}.png`;
+break;
+}
+}
+
+let rankCard = await Canvas.loadImage(dirImage);
+const pathImg = __root + `/rank_${id}.png`;
+
+var expWidth = (expCurrent * 615) / expNextLevel;
+if (expWidth > 615 - 18.5) expWidth = 615 - 18.5;
 	
-	console.log(await global.utils.assets.font("REGULAR-FONT"));
+ let avatar = await request.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`);
 
-	Canvas.registerFont(await global.utils.assets.font("REGULAR-FONT"), {
-		family: "Manrope",
-		weight: "regular",
-		style: "normal"
-	});
-	Canvas.registerFont(await global.utils.assets.font("BOLD-FONT"), {
-		family: "Manrope",
-		weight: "bold",
-		style: "normal"
-	});
+ avatar = await this.circle(avatar.body);
 
-	let rankCard = await Canvas.loadImage(await global.utils.assets.image("RANKCARD"));
-	const pathImg = __root + `/rank_${id}.png`;
-	
-	var expWidth = (expCurrent * 615) / expNextLevel;
-	if (expWidth > 615 - 18.5) expWidth = 615 - 18.5;
-	
-	let avatar = await request.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=${global.configModule[this.config.name].APIKEY}`);
+ const canvas = Canvas.createCanvas(934, 282);
+ const ctx = canvas.getContext("2d");
 
-	avatar = await this.circle(avatar.body);
+ ctx.drawImage(rankCard, 0, 0, canvas.width, canvas.height);
+ ctx.drawImage(await Canvas.loadImage(avatar), 45, 50, 180, 180);
 
-	const canvas = Canvas.createCanvas(934, 282);
-	const ctx = canvas.getContext("2d");
+ ctx.font = `bold 36px Manrope`;
+ ctx.fillStyle = "#FF66FF";
+ ctx.textAlign = "start";
+ ctx.fillText(name, 270, 164);
+ ctx.font = `36px Manrope`;
+ ctx.fillStyle = "#FF66FF";
+ ctx.textAlign = "center";
 
-	ctx.drawImage(rankCard, 0, 0, canvas.width, canvas.height);
-	ctx.drawImage(await Canvas.loadImage(avatar), 45, 50, 180, 180);
+ ctx.font = `bold 32px Manrope`;
+ ctx.fillStyle = "#550000";
+ ctx.textAlign = "end";
+ ctx.fillText(level, 934 - 55, 82);
+ ctx.fillStyle = "#550000";
+ ctx.fillText("Lv.", 934 - 55 - ctx.measureText(level).width - 10, 82);
 
-	ctx.font = `bold 36px Manrope`;
-	ctx.fillStyle = "#FFFFFF";
-	ctx.textAlign = "start";
-	ctx.fillText(name, 270, 164);
-	ctx.font = `36px Manrope`;
-	ctx.fillStyle = "#FFFFFF";
-	ctx.textAlign = "center";
+ ctx.font = `bold 32px Manrope`;
+ ctx.fillStyle = "#FF0000";
+ ctx.textAlign = "end";
+ ctx.fillText(rank, 934 - 55 - ctx.measureText(level).width - 16 - ctx.measureText(`Lv.`).width - 25, 82);
+ ctx.fillStyle = "#FF0000";
+ ctx.fillText("#", 934 - 55 - ctx.measureText(level).width - 16 - ctx.measureText(`Lv.`).width - 16 - ctx.measureText(rank).width - 16, 82);
 
-	ctx.font = `bold 32px Manrope`;
-	ctx.fillStyle = "#FFFFFF";
-	ctx.textAlign = "end";
-	ctx.fillText(level, 934 - 55, 82);
-	ctx.fillStyle = "#FFFFFF";
-	ctx.fillText("Lv.", 934 - 55 - ctx.measureText(level).width - 10, 82);
+ ctx.font = `bold 26px Manrope`;
+ ctx.fillStyle = "#00FF00";
+ ctx.textAlign = "start";
+ ctx.fillText("/ " + expNextLevel, 710 + ctx.measureText(expCurrent).width + 10, 164);
+ ctx.fillStyle = "#00FF00";
+ ctx.fillText(expCurrent, 710, 164);
 
-	ctx.font = `bold 32px Manrope`;
-	ctx.fillStyle = "#FFFFFF";
-	ctx.textAlign = "end";
-	ctx.fillText(rank, 934 - 55 - ctx.measureText(level).width - 16 - ctx.measureText(`Lv.`).width - 25, 82);
-	ctx.fillStyle = "#FFFFFF";
-	ctx.fillText("#", 934 - 55 - ctx.measureText(level).width - 16 - ctx.measureText(`Lv.`).width - 16 - ctx.measureText(rank).width - 16, 82);
+ ctx.beginPath();
+ ctx.fillStyle = "#FF6600";
+ ctx.arc(257 + 18.5, 147.5 + 18.5 + 36.25, 18.5, 1.5 * PI, 0.5 * PI, true);
+ ctx.fill();
+ ctx.fillRect(257 + 18.5, 147.5  + 36.25, expWidth, 37.5);
+ ctx.arc(257 + 18.5 + expWidth, 147.5 + 18.5 + 36.25, 18.75, 1.5 * PI, 0.5 * PI, false);
+ ctx.fill();
 
-	ctx.font = `bold 26px Manrope`;
-	ctx.fillStyle = "#FFFFFF";
-	ctx.textAlign = "start";
-	ctx.fillText("/ " + expNextLevel, 710 + ctx.measureText(expCurrent).width + 10, 164);
-	ctx.fillStyle = "#FFFFFF";
-	ctx.fillText(expCurrent, 710, 164);
-
-	ctx.beginPath();
-	ctx.fillStyle = "#4283FF";
-	ctx.arc(257 + 18.5, 147.5 + 18.5 + 36.25, 18.5, 1.5 * PI, 0.5 * PI, true);
-	ctx.fill();
-	ctx.fillRect(257 + 18.5, 147.5 + 36.25, expWidth, 37.5);
-	ctx.arc(257 + 18.5 + expWidth, 147.5 + 18.5 + 36.25, 18.75, 1.5 * PI, 0.5 * PI, false);
-	ctx.fill();
-
-	const imageBuffer = canvas.toBuffer();
-	fs.writeFileSync(pathImg, imageBuffer);
-	return pathImg;
+ const imageBuffer = canvas.toBuffer();
+ fs.writeFileSync(pathImg, imageBuffer);
+ return pathImg;
 }
 
 module.exports.circle = async (image) => {
     const jimp = global.nodemodule["jimp"];
-	image = await jimp.read(image);
-	image.circle();
-	return await image.getBufferAsync("image/png");
+ image = await jimp.read(image);
+ image.circle();
+ return await image.getBufferAsync("image/png");
 }
 
 module.exports.expToLevel = (point) => {
-	if (point < 0) return 0;
-	return Math.floor((Math.sqrt(1 + (4 * point) / 3) + 1) / 2);
+ if (point < 0) return 0;
+ return Math.floor((Math.sqrt(1 + (4 * point) / 3) + 1) / 2);
 }
 
 module.exports.levelToExp = (level) => {
-	if (level <= 0) return 0;
-	return 3 * level * (level - 1);
+ if (level <= 0) return 0;
+ return 3 * level * (level - 1);
 }
 
 module.exports.getInfo = async (uid, Currencies) => {
-	let point = (await Currencies.getData(uid)).exp;
-	const level = this.expToLevel(point);
-	const expCurrent = point - this.levelToExp(level);
-	const expNextLevel = this.levelToExp(level + 1) - this.levelToExp(level);
-	return { level, expCurrent, expNextLevel };
+ let point = (await Currencies.getData(uid)).exp;
+ const level = this.expToLevel(point);
+ const expCurrent = point - this.levelToExp(level);
+ const expNextLevel = this.levelToExp(level + 1) - this.levelToExp(level);
+ return { level, expCurrent, expNextLevel, EXP: point };
 }
 
-module.exports.languages = {
-	"vi": {
-		"userNotExist": "Bạn hiện không có trong cơ sở dữ liệu nên không thể thấy thứ hạng của mình, vui lòng thử lại sau 5 giây."
-	},
-	"en" :{
-		"userNotExist": "You are not currently in the database so you cannot see your rank, please try again in 5 seconds."
-	}
-}
 
-module.exports.run = async ({ event, api, Currencies, Users, getText }) => {
-	const { createReadStream, unlinkSync } = global.nodemodule["fs-extra"];
+module.exports.run = async ({ event, api, args, Currencies, Users }) => {
+ const fs = global.nodemodule["fs-extra"];
 	
-	let dataAll = (await Currencies.getAll(["userID", "exp"]));
+ let dataAll = (await Currencies.getAll(["userID", "exp"]));
+ const mention = Object.keys(event.mentions);
 
-	dataAll.sort(function (a, b) { return b.exp - a.exp });
+ dataAll.sort((a, b) => {
+  if (a.exp > b.exp) return -1;
+  if (a.exp < b.exp) return 1;
+ });
 
-	const rank = dataAll.findIndex(item => parseInt(item.userID) == parseInt(event.senderID)) + 1;
-	if (rank == 0) return api.sendMessage(getText("userNotExist"), event.threadID, event.messageID);
-	const name = await Users.getNameUser(event.senderID);
-	const point = await this.getInfo(event.senderID, Currencies);
-	const timeStart = Date.now();
-	let pathRankCard = await this.makeRankCard({ id: event.senderID, name, rank, ...point })
-	return api.sendMessage({body: `${Date.now() - timeStart}`, attachment: createReadStream(pathRankCard, {'highWaterMark': 128 * 1024}) }, event.threadID, () => unlinkSync(pathRankCard), event.messageID);
+ if (args.length == 0) {
+  const rank = dataAll.findIndex(item => parseInt(item.userID) == parseInt(event.senderID)) + 1;
+  const name = global.data.userName.get(event.senderID) || await Users.getNameUser(event.senderID);
+  if (rank == 0) return api.sendMessage("Bạn hiện không có trong cơ sở dữ liệu nên không thể thấy thứ hạng của mình, vui lòng thử lại sau 5 giây.", event.threadID, event.messageID);
+  const point = await this.getInfo(event.senderID, Currencies);
+  const timeStart = Date.now();
+  let pathRankCard = await this.makeRankCard({ id: event.senderID, name, rank, ...point })
+  return api.sendMessage({body: `📝 Số tin nhắn: ${point.EXP} ✅.` ,attachment: fs.createReadStream(pathRankCard, {'highWaterMark': 128 * 1024}) }, event.threadID, () => fs.unlinkSync(pathRankCard), event.messageID);
+ }
+ if (mention.length == 1) {
+  const rank = dataAll.findIndex(item => parseInt(item.userID) == parseInt(mention[0])) + 1;
+  const name = global.data.userName.get(mention[0]) || await Users.getNameUser(mention[0]);
+  if (rank == 0) return api.sendMessage("Bạn hiện không có trong cơ sở dữ liệu nên không thể thấy thứ hạng của mình, vui lòng thử lại sau 5 giây.", event.threadID, event.messageID);
+  let point = await this.getInfo(mention[0], Currencies);
+  let pathRankCard = await this.makeRankCard({ id: mention[0], name, rank, ...point })
+  return api.sendMessage({body: `📝${point.EXP} tin nhắn✅` ,attachment: fs.createReadStream(pathRankCard) }, event.threadID, () => fs.unlinkSync(pathRankCard), event.messageID);
+ }
+ if (mention.length > 1) {
+  for (const userID of mention) {
+   const rank = dataAll.findIndex(item => parseInt(item.userID) == parseInt(userID)) + 1;
+   const name = global.data.userName.get(userID) || await Users.getNameUser(userID);
+   if (rank == 0) return api.sendMessage("Bạn hiện không có trong cơ sở dữ liệu nên không thể thấy thứ hạng của mình, vui lòng thử lại sau 5 giây.", event.threadID, event.messageID);
+   let point = await this.getInfo(userID, Currencies);
+   let pathRankCard = await this.makeRankCard({ id: userID, name, rank, ...point })
+   return api.sendMessage({body:`📝${point.EXP} tin nhắn✅` ,attachment: fs.createReadStream(pathRankCard) }, event.threadID, () => fs.unlinkSync(pathRankCard), event.messageID);
+  }
+ }
 }
